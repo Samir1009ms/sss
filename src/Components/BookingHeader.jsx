36f5ca9cebe372d641655c8d238173e650a10f86
Samir1009ms <yusifovs1009@gmail.com>
2023-08-31 21:59:58 +0400

    s

---
+++ src/Components/BookingHeader.jsx
@@ -0,0 +1,16 @@
+
+// import rightvector from "../svg/rightvector.svg";
+import rightvector from "../svg/rightvector";
+function BookingHeader() {
+  return (
+    <div className="booking_detail_header">
+      <h4>Turkey</h4>
+      <img src={rightvector} alt="" />
+      <h4>Istanbul</h4>
+      <img src={rightvector} alt="" />
+      <h4>CVK Park Bosphorus Hotel Istanbul</h4>
+    </div>
+  )
+}
+
+export default BookingHeader
